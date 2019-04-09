ccc3f8b61f5baad5872cfacd39900987fe7ec130
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2019-04-09 20:59:02 +0700

    Prepare for upgrading CookieCutter Template use by this project

--- server/accounts/admin.py
+++ server/accounts/admin.py
@@ -1,36 +1,44 @@
-from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseAdmin
 from tenant_users.permissions.models import UserTenantPermissions
 from .models import TenantUser
-from .forms import UserChangeForm, UserCreationForm
 
 # Register your models here.
 
 
-class TenantUserAdmin(admin.ModelAdmin):
+class TenantUserAdmin(BaseAdmin):
 
     list_display = ('email', 'first_name', 'last_name','is_active','is_verified','last_login',)
     list_filter = ('is_active', 'is_verified', 'last_login',)
     search_fields = ('email', 'first_name', 'last_name',)
     readonly_fields = ('last_login',)
+    filter_horizontal = ()
+    ordering = ('email',)
     fieldsets = (
-        (None, {'fields': ('email', 'password', 'tenants') }),
-         ('Personal Info', {'fields': ('first_name', 'last_name') }),
-         ('Status', {'fields': ('is_active', 'is_verified', 'last_login') })
+        (None, {'fields': ('email', 'password', 'tenants')}),
+         ('Personal Info', {'fields': ('first_name', 'last_name')}),
+         ('Status', {'fields': ('is_active', 'is_verified', 'last_login')})
          )
 
-    def get_form(self, request, obj=None, **kwargs):
-        # Proper kwargs are form, fields, exclude, formfield_callback
-        if obj is None:
-            self.form = UserCreationForm
-        else:
-            self.form = UserChangeForm
-        return super(TenantUserAdmin, self).get_form(request, obj, **kwargs)
+    add_fieldsets = (
+        (None, {'fields': ('email', 'password1', 'password2', 'tenants') }),
+         ('Personal Info', {'fields': ('first_name', 'last_name') }),
+         ('Status', {'fields': ('is_active', 'is_verified')})
+         )
+
+    # def get_form(self, request, obj=None, **kwargs):
+    #    # Proper kwargs are form, fields, exclude, formfield_callback
+    #   if obj is None:
+    #        self.form = UserAdminCreationForm
+    #    else:
+    #        self.form = UserAdminChangeForm
+    #    return super(TenantUserAdmin, self).get_form(request, obj, **kwargs)
 
 
 @admin.register(UserTenantPermissions)
 class UserTenantPermissionsAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('profile', 'is_staff', 'is_superuser')
+
 
 
 admin.site.register(TenantUser, TenantUserAdmin)
